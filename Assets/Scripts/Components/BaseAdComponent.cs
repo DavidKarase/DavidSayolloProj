35d22bb39214c27d4186290b81b58084af9c9e97
davidk <susk123123@gmail.com>
2021-11-03 00:49:57 +0200

    BaseAdComponent

---
+++ Assets/Scripts/Components/BaseAdComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sayollo.Components
+{
+    public class BaseAdComponent : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            
+        }
+    }
+}
