0d259d817ae08d3502ca826790db072f7a9d9df5
davidk <susk123123@gmail.com>
2021-11-04 22:15:25 +0200

    ProductData

---
+++ Assets/Scripts/Core/Store/ProductData.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using UnityEngine;
+
+public class ProductData : MonoBehaviour
+{
+    [JsonProperty("title")]
+    public string Title;
+
+    [JsonProperty("item_id")]
+    public string ItemId;
+
+    [JsonProperty("item_name")]
+    public string ItemName;
+
+    [JsonProperty("item_image")]
+    public string ItemImage;
+
+    [JsonProperty("price")]
+    public double Price;
+
+    [JsonProperty("currency")]
+    public string Currency;
+
+    [JsonProperty("currency_sign")]
+    public string CurrencySign;
+
+    [JsonProperty("status")]
+    public string Status;
+
+    [JsonProperty("error_code")]
+    public int ErrorCode;
+}
